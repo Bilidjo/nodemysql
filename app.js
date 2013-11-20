a4a487824cd5a514ca80f1c384ef28e0e0453e84
Bilidjo <jamesbdlv@yahoo.fr>
2013-11-20 10:13:22 +0100

    initialisation du repository sans modules

---
+++ app.js
@@ -0,0 +1,69 @@
+// Module dependencies
+ 
+var express = require('express');
+var Client = require('mariasql');
+//mysql = require('mysql');
+ 
+/* Application initialization
+ 
+var connection = mysql.createConnection({
+host : 'localhost',
+user : 'root',
+password : 'pizzle'
+});*/
+
+var connection = new Client();
+connection.connect
+(
+  {
+      host: '127.0.0.1',
+      port: '3307',
+      user: 'root',
+      password: 'pizzle'
+  }
+);
+
+
+var app = express();
+ 
+// Database setup
+ 
+connection.query('CREATE DATABASE IF NOT EXISTS test', function (err) {
+if (err) throw err;
+connection.query('USE test', function (err) {
+if (err) throw err;
+connection.query('CREATE TABLE IF NOT EXISTS users('
++ 'id INT NOT NULL AUTO_INCREMENT,'
++ 'PRIMARY KEY(id),'
++ 'name VARCHAR(30)'
++ ')', function (err) {
+if (err) throw err;
+});
+});
+});
+ 
+// Configuration
+ 
+app.use(express.bodyParser());
+ 
+// Main route sends our HTML file
+ 
+app.get('/', function(req, res) {
+res.sendfile(__dirname + '/index.html');
+});
+ 
+// Update MySQL database
+ 
+app.post('/users', function (req, res) {
+connection.query('INSERT INTO users SET ?', req.body,
+function (err, result) {
+if (err) throw err;
+res.send('User added to database with ID: ' + result.insertId);
+}
+);
+});
+ 
+// Begin listening
+ 
+app.listen(3000);
+console.log("Express server listening on port 3000 in %s mode", app.settings.env);
